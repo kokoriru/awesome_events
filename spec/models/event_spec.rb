65028612bdc9f2ecba2876b81658ba41e71011cc
kokoriru <kokorirury@yahoo.co.jp>
2016-04-25 16:06:57 +0900

    モデルのテスト作成

---
+++ spec/models/event_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Event, :type => :model do
+  describe '#name' do
+    context '空白のとき' do
+      let(:event) { Event.new(name: '') }
+
+      it 'valid でないこと' do
+        event = Event.new(name: '')
+        event.valid?
+        expect(event.errors[:name]).to be_present
+      end
+    end
+  end
+end
